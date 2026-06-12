b46a49845a9b19030d7c69f1d096a4ecf845c79e
Tyson Harding <tysonharding96@gmail.com>
2026-06-12 13:44:32 +1200

    gg

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,7 @@
 'use client';
 
 import { Canvas } from '@react-three/fiber';
-
-type Position = [number, number, number];
-
-function ShaderPlane({ position, color1, color2 }: { position?: Position; color1: string; color2: string }) {
-  return (
-    <mesh position={position}>
-      <planeGeometry args={[4, 4]} />
-      <meshBasicMaterial color={color1} emissive={color2} />
-    </mesh>
-  );
-}
-
-function EnergyRing({ radius, position }: { radius: number; position?: Position }) {
-  return (
-    <mesh position={position}>
-      <torusGeometry args={[radius, 0.05, 16, 64]} />
-      <meshBasicMaterial color="#00e5ff" wireframe />
-    </mesh>
-  );
-}
+import { ShaderPlane, EnergyRing } from '@/components/ShaderPlane';
 
 export default function Home() {
   return (
